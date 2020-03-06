324b5f3ebdb654a9099dd343284619513ae82f17
John Rake <john.rake12@gmail.com>
2020-03-05 18:49:51 -0600

    add client build and scripts

---
+++ client/build/precache-manifest.d016c2db07dba5e8400aa4e655ecdba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d37a34691e904c54b13c90ee5416c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc7c4fa90d2cdb6e3b6",
+    "url": "/static/js/2.4378846c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4378846c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf7bb74db842788ebb2",
+    "url": "/static/js/main.35477ca7.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
